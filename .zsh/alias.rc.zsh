6cdefd150e9a26ef998ab800b1f6b00dca5e59c5
Hayato KAWAI <g@fohte.net>
2016-07-03 16:08:35 +0900

    [zsh] fix gaf alias

--- .zsh/alias.rc.zsh
+++ .zsh/alias.rc.zsh
@@ -29,7 +29,7 @@ gaf() {
   local addfiles
   addfiles=($(git status --short | grep -v '##' | awk '{ print $2 }' | fzf-tmux --multi))
   if [[ -n $addfiles ]]; then
-    git add ${@:2} $addfiles && echo "added: $addfiles"
+    git add ${@:1} $addfiles && echo "added: $addfiles"
   else
     echo "nothing added."
   fi
