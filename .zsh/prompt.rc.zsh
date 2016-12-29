a18ccfb201246fb3ab01b4948de79a36845e786e
Hayato Kawai <fohte.hk@gmail.com>
2016-12-29 19:27:45 +0900

    [zsh] Update prompt

--- .zsh/prompt.rc.zsh
+++ .zsh/prompt.rc.zsh
@@ -1,8 +1,25 @@
-dir="%{$fg[cyan]%}%(5~,.../%3~,%~)%{$reset_color%}"
-privileges="%{$fg[red]%}%#%{$reset_color%}"
-host="%m"
-user="%n"
-machine_info="%{$fg[magenta]%}$user@$host%{$reset_color%}"
+setopt prompt_subst
 
-PROMPT="$machine_info $privileges "
-RPROMPT="$dir"
+git_current_branch() {
+  local branch
+  branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+  if [ -z $branch ]; then
+    echo -n ""
+  else
+    echo -n "[${branch}]"
+  fi
+}
+
+dynamic_privilege() {
+  local status_color
+  status_color='blue'
+  echo -n "%{$fg[$status_color]%}>%{$reset_color%}>"
+}
+
+dir='%{$fg[magenta]%}%~%{$reset_color%}'
+branch='$(git_current_branch)'
+privileges='$(dynamic_privilege)'
+
+PROMPT="
+$dir $branch
+$privileges "
