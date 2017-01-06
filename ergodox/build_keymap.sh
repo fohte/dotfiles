1a6d100f43169e7c1b89b52fcf3722b9f6e6b3ad
Hayato Kawai <fohte.hk@gmail.com>
2017-01-07 01:11:06 +0900

    ergodox: Move keymap.c into src

--- ergodox/build_keymap.sh
+++ ergodox/build_keymap.sh
@@ -4,17 +4,18 @@ KEYMAP=fohte
 KEYBOARD=ergodox
 SUB=ez
 
-pwd_dir=$(dirname $0)
-cd "$pwd_dir"
+cd $(dirname $0)
+pwd_dir=$(pwd)
 
-QMK_DIR="qmk"
-MY_KEYMAP_C="keymap.c"
+QMK_DIR="$pwd_dir/qmk"
+MY_KEYMAP_DIR="$pwd_dir/src"
 QMK_KEYMAP_DIR="${QMK_DIR}/keyboards/ergodox/keymaps/${KEYMAP}"
+
 HEX_FILE="${KEYBOARD}_${SUB}_${KEYMAP}.hex"
 
 [ ! -e "$QMK_DIR" ] && git clone https://github.com/fohte/qmk_firmware.git "$QMK_DIR"
-mkdir -p "$QMK_KEYMAP_DIR"
-cp -f "$MY_KEYMAP_C" "$QMK_KEYMAP_DIR/keymap.c"
+rm -rf "$QMK_KEYMAP_DIR"
+cp -rf "$MY_KEYMAP_DIR" "$QMK_KEYMAP_DIR"
 
-docker run --rm -e keymap=$KEYMAP -e keyboard=$KEYBOARD -v "$('pwd')/$QMK_DIR":/qmk:rw -t jackhumbert/qmk_firmware /bin/bash -c 'make' || exit 1
-cp -f "$QMK_DIR/$HEX_FILE" "$HEX_FILE" && echo "Created hex file: $pwd_dir/$HEX_FILE"
+docker run --rm -e keymap=$KEYMAP -e keyboard=$KEYBOARD -v "$QMK_DIR":/qmk:rw -t jackhumbert/qmk_firmware /bin/bash -c 'make' || exit 1
+cp -f "$QMK_DIR/$HEX_FILE" "$pwd_dir/$HEX_FILE" && echo "Created hex file: $pwd_dir/$HEX_FILE"
