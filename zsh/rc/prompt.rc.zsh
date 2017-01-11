a19641e5a3f7ce697db94aeff39390101497399b
Hayato Kawai <fohte.hk@gmail.com>
2017-01-12 00:18:53 +0900

    zsh: Make prompt color change by vi-mode

--- zsh/rc/prompt.rc.zsh
+++ zsh/rc/prompt.rc.zsh
@@ -1,5 +1,11 @@
 setopt prompt_subst
 
+zle-line-init zle-keymap-select() {
+ zle reset-prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
+
 git_current_branch() {
   local branch
   branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
@@ -12,12 +18,24 @@ git_current_branch() {
 
 dynamic_privilege() {
   local status_color
+  local vimode_color
+
   if is_ssh_running; then
     status_color='red'
   else
     status_color='blue'
   fi
-  echo -n "%{$fg[$status_color]%}>%{$reset_color%}>"
+
+  case $KEYMAP in
+    vicmd)
+      vimode_color=''
+      ;;
+    viins|main)
+      vimode_color='blue'
+      ;;
+  esac
+
+  echo -n "${fg[$status_color]}>${reset_color}${fg[$vimode_color]}>${reset_color}"
 }
 
 dir='%{$fg[magenta]%}%~%{$reset_color%}'
