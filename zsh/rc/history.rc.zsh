80816fac9b1b3ea67bcca28d2dc3a82fd7f6edc5
Hayato Kawai <fohte.hk@gmail.com>
2017-01-20 14:19:51 +0900

    zsh: Don't save 4 or less letters command to history

--- zsh/rc/history.rc.zsh
+++ zsh/rc/history.rc.zsh
@@ -1,3 +1,10 @@
 setopt hist_ignore_dups
 setopt hist_reduce_blanks
 setopt share_history
+
+zshaddhistory() {
+  local line=${1%%$'\n'}
+
+  # Ignore 4 or less letters command
+  [[ ${#line} -ge 5 ]]
+}
