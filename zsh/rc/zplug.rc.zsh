603438ca0175e69060329e950a878ae7731be9ae
Hayato Kawai <fohte.hk@gmail.com>
2017-10-10 00:45:47 +0900

    [zsh] Add spaceship theme and remove my prompt theme

--- zsh/rc/zplug.rc.zsh
+++ zsh/rc/zplug.rc.zsh
@@ -3,6 +3,7 @@ source ~/.zplug/init.zsh
 zplug 'zsh-users/zsh-completions'
 zplug 'zsh-users/zsh-syntax-highlighting', defer:2
 zplug 'b4b4r07/enhancd', use:init.sh
+zplug 'denysdovhan/spaceship-zsh-theme', use:spaceship.zsh, from:github, as:theme
 
 if ! zplug check --verbose; then
   printf 'Install? [y/N]: '
