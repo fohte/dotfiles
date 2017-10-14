570f1d5925d96ea4dd41b7f2718197044ebbd05b
Hayato Kawai <fohte.hk@gmail.com>
2017-10-15 00:20:01 +0900

    [zsh] Change the cursor shape for vi-modes

--- zsh/rc/misc.rc.zsh
+++ zsh/rc/misc.rc.zsh
@@ -5,3 +5,20 @@ setopt menu_complete
 
 setopt ignore_eof
 setopt rm_star_wait
+
+zle-line-init zle-keymap-select() {
+  if [ ! "$TERM" = "xterm-256color" ]; then
+    return
+  fi
+
+  case $KEYMAP in
+    vicmd)
+      echo -ne "\e[2 q" # block
+      ;;
+    main|viins)
+      echo -ne "\e[6 q" # vertical bar
+      ;;
+  esac
+}
+zle -N zle-line-init
+zle -N zle-keymap-select
