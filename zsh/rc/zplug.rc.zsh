c4d6963e8d855afc09dc2458c2928792cf73a7a8
Hayato Kawai <fohte.hk@gmail.com>
2017-10-14 23:23:35 +0900

    [zsh] Use zplug to install pure theme

--- zsh/rc/zplug.rc.zsh
+++ zsh/rc/zplug.rc.zsh
@@ -3,6 +3,8 @@ source ~/.zplug/init.zsh
 zplug 'zsh-users/zsh-completions'
 zplug 'zsh-users/zsh-syntax-highlighting', defer:2
 zplug 'b4b4r07/enhancd', use:init.sh
+zplug 'mafredri/zsh-async', from:github
+zplug 'sindresorhus/pure', use:pure.zsh, from:github, as:theme
 
 if ! zplug check --verbose; then
   printf 'Install? [y/N]: '
