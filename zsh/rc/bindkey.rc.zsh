be206f84108d19181b16e0f2c93995d92fb31213
Hayato Kawai <fohte.hk@gmail.com>
2017-10-15 16:29:54 +0900

    [zsh] Split up to bindkey/{vimode,fzf}.rc.zsh

--- zsh/rc/bindkey.rc.zsh
+++ zsh/rc/bindkey.rc.zsh
@@ -1,33 +1,4 @@
-# vi mode settings
-bindkey -v
 bindkey '^?' backward-delete-char
 
-bindkey -r '^G'
-
-fzf-git-nocommit-file() {
-  local selected
-  selected=($(git status -s | fzf-tmux -m --ansi --preview "git diff --color \$(echo {} | awk '{ print \$2 }')" | awk '{ print $2 }'))
-  LBUFFER="${LBUFFER}${selected}"
-}
-zle -N fzf-git-nocommit-file
-bindkey '^G^T' fzf-git-nocommit-file
-
-fzf-git-log() {
-  local selected
-  selected=($(git log --pretty=format:'%C(yellow)%h%C(reset) %s %C(green)%an%C(reset)' | fzf-tmux --ansi --preview "git diff --color \$(echo {} | awk '{ print \$1 \"^ \" \$1 }')" | awk '{ print $1 }'))
-  LBUFFER="${LBUFFER}${selected}"
-}
-zle -N fzf-git-log
-bindkey '^G^L' fzf-git-log
-
-fzf-git-branch() {
-  selected=$(
-    git branch -a | \
-    fzf-tmux --ansi | \
-    sed -e 's/^* //g' | \
-    awk '{ print $1 }'
-  )
-  LBUFFER="${LBUFFER}${selected}"
-}
-zle -N fzf-git-branch
-bindkey '^G^B' fzf-git-branch
+source_rc 'bindkey/vimode.rc.zsh'
+source_rc 'bindkey/fzf.rc.zsh'
