093f1ed9c593135e48f1c66de7ce37c4f62aa941
Hayato Kawai <fohte.hk@gmail.com>
2017-10-15 17:06:21 +0900

    [zsh] Insert a space automatically by fzf binding commands

--- zsh/rc/bindkey/fzf.rc.zsh
+++ zsh/rc/bindkey/fzf.rc.zsh
@@ -1,9 +1,21 @@
 bindkey -r '^G'
 
+_append_to_lbuffer() {
+  if [ "${LBUFFER[-1]}" != ' ' ]; then
+    LBUFFER="${LBUFFER} "
+  fi
+  LBUFFER="${LBUFFER}${1}"
+}
+
 fzf-git-nocommit-file() {
   local selected
   selected=($(git status -s | fzf-tmux -m --ansi --preview "git diff --color \$(echo {} | awk '{ print \$2 }')" | awk '{ print $2 }'))
-  LBUFFER="${LBUFFER}${selected}"
+
+  if [ -z "${selected}" ]; then
+    return
+  fi
+
+  _append_to_lbuffer "${selected}"
 }
 zle -N fzf-git-nocommit-file
 bindkey '^G^T' fzf-git-nocommit-file
@@ -11,7 +23,12 @@ bindkey '^G^T' fzf-git-nocommit-file
 fzf-git-log() {
   local selected
   selected=($(git log --pretty=format:'%C(yellow)%h%C(reset) %s %C(green)%an%C(reset)' | fzf-tmux --ansi --preview "git diff --color \$(echo {} | awk '{ print \$1 \"^ \" \$1 }')" | awk '{ print $1 }'))
-  LBUFFER="${LBUFFER}${selected}"
+
+  if [ -z "${selected}" ]; then
+    return
+  fi
+
+  _append_to_lbuffer "${selected}"
 }
 zle -N fzf-git-log
 bindkey '^G^L' fzf-git-log
@@ -23,7 +40,12 @@ fzf-git-branch() {
     sed -e 's/^* //g' | \
     awk '{ print $1 }'
   )
-  LBUFFER="${LBUFFER}${selected}"
+
+  if [ -z "${selected}" ]; then
+    return
+  fi
+
+  _append_to_lbuffer "${selected}"
 }
 zle -N fzf-git-branch
 bindkey '^G^B' fzf-git-branch
