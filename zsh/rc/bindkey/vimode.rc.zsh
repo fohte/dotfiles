2d019913eee7c18cf511c826ec04f5d724614c53
Hayato Kawai <fohte.hk@gmail.com>
2017-10-16 00:25:13 +0900

    [zsh] Update vi-mode bindings

--- zsh/rc/bindkey/vimode.rc.zsh
+++ zsh/rc/bindkey/vimode.rc.zsh
@@ -1,1 +1,28 @@
 bindkey -v
+
+autoload -Uz surround
+
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+
+bindkey -a -r s
+bindkey -a sr change-surround
+bindkey -a sd delete-surround
+bindkey -a sa add-surround
+
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+    bindkey -M $m $c select-bracketed
+  done
+done
+
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+  for c in {a,i}{\',\",\`}; do
+    bindkey -M $m $c select-quoted
+  done
+done
