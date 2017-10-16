6cf3324e14ef5f6439b22c2c57ac7424496d96c4
Hayato Kawai <fohte.hk@gmail.com>
2017-10-16 13:28:57 +0900

    [zsh] Add zsh-vimode-visual

--- zsh/rc/zplug.rc.zsh
+++ zsh/rc/zplug.rc.zsh
@@ -6,6 +6,7 @@ zplug 'zsh-users/zsh-syntax-highlighting', defer:2
 zplug 'b4b4r07/enhancd', use:init.sh
 zplug 'mafredri/zsh-async', from:github
 zplug 'sindresorhus/pure', use:pure.zsh, from:github, as:theme
+zplug 'b4b4r07/zsh-vimode-visual', defer:3
 
 if ! zplug check --verbose; then
   printf 'Install? [y/N]: '
