b38848680d31899eca8012c088c319bca2b97fec
Hayato Kawai <fohte.hk@gmail.com>
2017-10-17 21:59:05 +0900

    [zsh] Remove enhancd

--- zsh/rc/zplug.rc.zsh
+++ zsh/rc/zplug.rc.zsh
@@ -3,7 +3,6 @@ source ~/.zplug/init.zsh
 zplug 'zsh-users/zsh-completions'
 zplug 'zsh-users/zsh-autosuggestions'
 zplug 'zsh-users/zsh-syntax-highlighting', defer:2
-zplug 'b4b4r07/enhancd', use:init.sh
 zplug 'mafredri/zsh-async', from:github
 zplug 'sindresorhus/pure', use:pure.zsh, from:github, as:theme
 zplug 'b4b4r07/zsh-vimode-visual', defer:3
