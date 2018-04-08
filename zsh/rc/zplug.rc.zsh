75e032962a1a6d3fc738692f008f6675e68df52c
Hayato Kawai <fohte.hk@gmail.com>
2018-04-08 21:27:28 +0900

    chore(zsh/zplug): add opp.zsh

--- zsh/rc/zplug.rc.zsh
+++ zsh/rc/zplug.rc.zsh
@@ -6,6 +6,7 @@ zplug 'zsh-users/zsh-syntax-highlighting', defer:2
 zplug 'mafredri/zsh-async', from:github
 zplug 'sindresorhus/pure', use:pure.zsh, from:github, as:theme
 zplug 'b4b4r07/zsh-vimode-visual', defer:3
+zplug 'hchbaw/opp.zsh'
 
 if ! zplug check --verbose; then
   printf 'Install? [y/N]: '
