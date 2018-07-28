d066c7d454f47b8a3202d1d04b666fdb36290575
Hayato Kawai <fohte.hk@gmail.com>
2018-07-28 22:46:27 +0900

    install: allow path including whitespaces

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+set -e
+
 abspath() {
-  echo "$(cd $(dirname $1) && pwd)/$(basename $1)"
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
 cd "$(dirname "$0")"
 
 cat .symlinks | grep -v '^$' | while read link; do
   read from to <<< $link
-  to=$(eval echo $to)
-  [ ! -d $to ] && mkdir -p "$(dirname "$to")"
-  from=$(abspath $from)
-  to=$(abspath $to)
-  ln -snv $from $to
+  to="$(eval echo "$to")"
+  to_dir="$(dirname "$to")"
+  [ ! -d "$to_dir" ] && mkdir -p "$to_dir"
+  from="$(abspath "$from")"
+  to="$(abspath "$to")"
+  ln -snv "$from" "$to"
 done
--- uninstall.sh
+++ uninstall.sh
@@ -3,11 +3,11 @@
 set -e
 
 abspath() {
-  echo "$(cd $(dirname $1) && pwd)/$(basename $1)"
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
 cat .symlinks | grep -v '^$' | while read link; do
   read from to <<< $link
-  to=$(eval abspath $to)
-  [ -e "$to" ] && unlink $to && echo unlink $to
+  to="$(eval abspath "$to")"
+  [ -e "$to" ] && unlink "$to" && echo unlink "$to"
 done
