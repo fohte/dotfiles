84ada1f9a905140fbcc89e366b44d61bc92bf649
Hayato Kawai <fohte.hk@gmail.com>
2018-10-08 16:23:18 +0900

    install: allow arguments to pass them to ansible-playbook

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 
 scripts/install_ansible.sh
 
-ansible-playbook -i ./hosts ./local_env.yml --ask-become-pass -vv
+ansible-playbook -i ./hosts ./local_env.yml --ask-become-pass -vv "$@"
