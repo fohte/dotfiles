b9a39279404341df0ef53aaf93b4a67682bc9a9c
Hayato Kawai <fohte.hk@gmail.com>
2019-01-24 20:23:00 +0900

    zsh: show information for an AWS assuming role on the prompt

--- roles/zsh/files/.zsh/prompt.rc.zsh
+++ roles/zsh/files/.zsh/prompt.rc.zsh
@@ -1,1 +1,5 @@
+aws_account_info() {
+  [ -n "$AWS_ACCOUNT_NAME" ] && [ -n "$AWS_ACCOUNT_ROLE" ] && echo "%F{magenta}$AWS_ACCOUNT_NAME:$AWS_ACCOUNT_ROLE%f%{$reset_color%} "
+}
 
+PROMPT='$(aws_account_info)'"$PROMPT"
