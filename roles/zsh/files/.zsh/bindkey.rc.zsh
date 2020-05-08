08608a844180ddeb800f76feb29a229d08160062
Hayato Kawai <fohte.hk@gmail.com>
2020-05-08 10:58:58 +0900

    zsh: fix import_rc

--- roles/zsh/files/.zsh/bindkey.rc.zsh
+++ roles/zsh/files/.zsh/bindkey.rc.zsh
@@ -1,7 +1,7 @@
 bindkey '^?' backward-delete-char
 
-import_rc 'bindkey/vimode'
-import_rc 'bindkey/fzf'
+import_rc 'bindkey/vimode.rc.zsh'
+import_rc 'bindkey/fzf.rc.zsh'
 
 bindkey '^P' up-history
 bindkey '^N' down-history
