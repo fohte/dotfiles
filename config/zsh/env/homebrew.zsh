cab2ffbd5d3d7d526f0726ea2b3155fcdc5f232a
Hayato Kawai <fohte.hk@gmail.com>
2022-01-11 17:08:27 +0900

    zsh: fix homebrew (linuxbrew)
    fix following error on launch zsh:
    
    ```
    /home/***/.config/zsh/env/homebrew.zsh:1: no such file or directory: /opt/homebrew/bin/brew
    ```

--- config/zsh/env/homebrew.zsh
+++ config/zsh/env/homebrew.zsh
@@ -1,1 +1,9 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# for Linuxbrew (Homebrew on Linux)
+if [ -d "$HOME/.linuxbrew" ]; then
+  eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"
+fi
+
+# for Homebrew (macOS)
+if [ -f /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
