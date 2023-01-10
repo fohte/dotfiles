1df3b7a21e3162757c282a64fc47e6a2700d4eb9
Hayato Kawai <fohte.hk@gmail.com>
2023-01-10 01:57:00 +0000

    zsh: fix pure.zsh settings
    https://github.com/sindresorhus/pure/commit/30fa78f883b51086558b45ce2914863d620c0a6a

--- config/zsh/rc/zinit.rc.zsh
+++ config/zsh/rc/zinit.rc.zsh
@@ -4,7 +4,7 @@ zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-syntax-highlighting
 
-zinit ice pick'async.zsh' src'pure.zsh'
+zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
 zinit light sindresorhus/pure
 
 zinit light b4b4r07/zsh-vimode-visual
