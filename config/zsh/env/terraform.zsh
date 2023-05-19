93dc84fafd0ec53c5ed3a84baacb36f0e86144b8
Hayato Kawai <fohte.hk@gmail.com>
2023-05-19 13:14:54 +0900

    zsh: create TF_PLUGIN_CACHE_DIR if not exists

--- config/zsh/env/terraform.zsh
+++ config/zsh/env/terraform.zsh
@@ -1,5 +1,8 @@
 # a cache directory for Terraform plugins
 export TF_PLUGIN_CACHE_DIR=~/.cache/terraform/plugins
+if ! [ -d "$TF_PLUGIN_CACHE_DIR" ]; then
+  mkdir -p "$TF_PLUGIN_CACHE_DIR"
+fi
 
 # increase parallelism form 10 from 20
 export TF_CLI_ARGS_plan="-parallelism=20"
