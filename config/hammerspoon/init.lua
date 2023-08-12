6daba583f0be799350aa7339c31affb0028ef29c
Hayato Kawai <fohte.hk@gmail.com>
2023-08-12 17:59:49 +0900

    hammerspoon: use hammerspoon to define shortcut for switching apps

---
+++ config/hammerspoon/init.lua
@@ -0,0 +1,11 @@
+hs.hotkey.bind({"alt"}, "1", function()
+  hs.application.launchOrFocus("/Applications/WezTerm.app")
+end)
+
+hs.hotkey.bind({"alt"}, "2", function()
+  hs.application.launchOrFocus("/Applications/Arc.app")
+end)
+
+hs.hotkey.bind({"alt"}, "0", function()
+  hs.application.launchOrFocus("/Applications/Obsidian.app")
+end)
