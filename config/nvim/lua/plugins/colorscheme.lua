67fb724e5895e20748954af36ab678892a37cc2e
Hayato Kawai <fohte.hk@gmail.com>
2023-08-21 03:02:30 +0900

    nvim: dim vertical line

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,7 @@ return {
     vim.api.nvim_set_hl(0, 'Search', { link = 'IncSearch' })
     vim.api.nvim_set_hl(0, 'MatchParen', { link = 'Number' })
     vim.api.nvim_set_hl(0, 'LineNr', { fg = '#444444' })
+    vim.api.nvim_set_hl(0, 'VertSplit', { fg = '#444444' })
 
     -- make transparent background (use terminal bacgkground color)
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
