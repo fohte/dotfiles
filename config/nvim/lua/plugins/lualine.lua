1790cd249215d98d9fe626a2cdb424e9d7815de5
Hayato Kawai <fohte.hk@gmail.com>
2023-08-21 02:40:02 +0900

    nvim: update lualine settings

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,21 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'RRethy/nvim-base16',
+  },
   config = function()
     require('lualine').setup({
       options = {
         theme = 'base16',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'filename' },
+        lualine_c = { },
+        lualine_x = { 'encoding' },
+        lualine_y = { 'branch', 'diff' },
+        lualine_z = { 'filetype' },
       },
     })
   end,
