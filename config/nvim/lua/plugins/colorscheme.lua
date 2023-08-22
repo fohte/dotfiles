33edb5b55cdb8f9c8279ce6867c34c4406c4b13d
Hayato Kawai <fohte.hk@gmail.com>
2023-08-23 03:38:27 +0900

    nvim: make transparent some highlights

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
   'RRethy/nvim-base16',
   config = function()
+    local utils = require('utils')
+
     vim.cmd.colorscheme('base16-material-darker')
 
     vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#444444', fg = 'none' })
@@ -10,6 +12,14 @@ return {
     vim.api.nvim_set_hl(0, 'VertSplit', { fg = '#444444' })
 
     -- make transparent background (use terminal bacgkground color)
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    local transparent_targets = {
+      'Normal',
+      'NormalNC',
+      'SignColumn',
+    }
+    for _, target in ipairs(transparent_targets) do
+      local current = vim.api.nvim_get_hl(0, { name = target })
+      vim.api.nvim_set_hl(0, target, utils.mergeTables(current, { bg = 'none' }))
+    end
   end,
 }
---
+++ config/nvim/lua/utils.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+-- Merges two tables together, overwriting the first table with the second.
+-- e.g. mergeTables({a = 1, b = 2}, {b = 3, c = 4}) -> {a = 1, b = 3, c = 4}
+function M.mergeTables(t1, t2)
+  for k, v in pairs(t2) do
+    if type(v) == "table" then
+      if type(t1[k] or false) == "table" then
+        mergeTables(t1[k] or {}, t2[k] or {})
+      else
+        t1[k] = v
+      end
+    else
+      t1[k] = v
+    end
+  end
+  return t1
+end
+
+return M
