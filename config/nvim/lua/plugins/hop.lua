48411e8adcaf38556e721aa614f35238440cb3a6
Hayato Kawai <fohte.hk@gmail.com>
2023-08-25 02:32:58 +0900

    nvim: add hop.nvim

---
+++ config/nvim/lua/plugins/hop.lua
@@ -0,0 +1,15 @@
+return {
+  'phaazon/hop.nvim',
+  config = function()
+    require('hop').setup({
+      keys = 'etuhonas.c,rpg',
+    })
+  end,
+  keys = function()
+    return {
+      { '<Leader>c', function() require('hop').hint_char1() end },
+      { '<Leader>l', function() require('hop').hint_lines_skip_whitespace() end },
+      { '<Leader>w', function() require('hop').hint_words() end },
+    }
+  end,
+}
