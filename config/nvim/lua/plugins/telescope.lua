36b8a10b572a8f104a934854675cd02edbf04058
Hayato Kawai <fohte.hk@gmail.com>
2023-08-24 13:04:54 +0900

    nvim: setup telescope-fzf-native

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
   keys = {
     { '<Leader>ep', '<cmd>Telescope find_files<cr>', mode = 'n' },
     { '<Leader>ef', '<cmd>Telescope find_files<cr>', mode = 'n' },
@@ -8,4 +11,18 @@ return {
     { '<Leader>eg', '<cmd>Telescope live_grep<cr>', mode = 'n'  },
     { '<Leader>eb', '<cmd>Telescope buffers<cr>', mode = 'n'  },
   },
+  config = function()
+    require('telescope').setup{
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        }
+      }
+    }
+
+    require('telescope').load_extension('fzf')
+  end,
 }
