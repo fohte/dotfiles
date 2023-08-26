2037003bf6b6fafa83c3fcefa3817e241330777a
Hayato Kawai <fohte.hk@gmail.com>
2023-08-27 00:56:59 +0900

    Revert "nvim: add noice.nvim"
    This reverts commit 1e6abdda309747eb7234308c8a8e4bb21c40741d.

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,14 +5,10 @@ return {
   config = function()
     require'nvim-treesitter.configs'.setup {
       ensure_installed = {
-        "bash",
         "javascript",
         "json",
         "json5",
         "lua",
-        "markdown",
-        "markdown_inline",
-        "regex",
         "tsx",
         "typescript",
         "vim",
