1923a2a66e11405a353490d3f65b3eeed2ec9916
Hayato Kawai <fohte.hk@gmail.com>
2023-09-20 15:24:46 +0900

    hammerspoon: assign Alt-9 to open logseq

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -14,6 +14,10 @@ hs.hotkey.bind({ 'alt' }, '2', function()
   hs.application.launchOrFocus('/Applications/Arc.app')
 end)
 
+hs.hotkey.bind({ 'alt' }, '9', function()
+  hs.application.launchOrFocus('/Applications/Logseq.app')
+end)
+
 hs.hotkey.bind({ 'alt' }, '0', function()
   hs.application.launchOrFocus('/Applications/Obsidian.app')
 end)
