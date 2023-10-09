062e191c079db19e971bd429bf59db117d879698
Hayato Kawai <fohte.hk@gmail.com>
2023-10-09 15:32:27 +0900

    nvim: add nvim-cmp

---
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    'hrsh7th/nvim-cmp',
+    event = { 'InsertEnter' },
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'quangnguyen30192/cmp-nvim-ultisnips',
+    },
+    config = function()
+      local cmp = require('cmp')
+      local map = cmp.mapping
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn['UltiSnips#Anon'](args.body) -- For `ultisnips` users.
+          end,
+        },
+
+        mapping = map.preset.insert({
+          ['<C-b>'] = map.scroll_docs(-4),
+          ['<C-f>'] = map.scroll_docs(4),
+          ['<C-e>'] = map.abort(),
+          ['<C-s>'] = map.confirm({ select = true }),
+        }),
+
+        sources = cmp.config.sources(
+          {
+            { name = 'nvim_lsp' },
+            { name = 'ultisnips' },
+          },
+
+          -- if lsp is not available, use buffer
+          {
+            { name = 'buffer' },
+          }
+        ),
+      })
+
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+        }, {
+          { name = 'cmdline' },
+        }),
+      })
+    end,
+  },
+}
