a40016390fd11c69e83e18bfefee7cf1b29d945e
Hayato Kawai <fohte.hk@gmail.com>
2024-04-17 11:17:54 +0900

    nvim/oil: show hidden files by default

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -5,4 +5,11 @@ return {
   keys = {
     { '<Leader>eo', ':Oil<CR>', mode = 'n', silent = true },
   },
+  config = function()
+    require('oil').setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
+  end,
 }
