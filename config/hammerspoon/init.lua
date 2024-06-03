5f9c35b62f1232f00bfc6279d89e175e63a77724
Hayato Kawai <fohte.hk@gmail.com>
2024-06-04 03:09:28 +0900

    hammerspoon: add shortcuts to resize windows

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -67,3 +67,111 @@ hs.hotkey.bind({ 'alt' }, 'a', function()
     run_command(yabai .. ' -m config focus_follows_mouse off')
   end
 end)
+
+-- Disable animation
+hs.window.animationDuration = 0
+
+local lib = {}
+lib.window = {}
+
+function lib.window:resizeWindow(fn)
+  local window = hs.window.focusedWindow()
+  local f = window:frame()
+  local max = window:screen():frame()
+
+  fn(f, max)
+
+  window:setFrame(f)
+end
+
+-- 1/2 of the left side
+function lib.window:first_half(max)
+  local r = hs.geometry.rect(max.x, max.y, max.w, max.h)
+  r.w = math.floor(max.w / 2)
+  return r
+end
+
+-- 1/2 of the right side
+function lib.window:last_half(max)
+  local r = hs.geometry.rect(max.x, max.y, max.w, max.h)
+  r.x = math.floor(max.x + max.w / 2)
+  r.w = math.floor(max.w / 2)
+  return r
+end
+
+-- 1/3 of the left side
+function lib.window:first_third(max)
+  local r = hs.geometry.rect(max.x, max.y, max.w, max.h)
+  r.w = math.floor(max.w / 3)
+  return r
+end
+
+-- 1/3 of the right side
+function lib.window:last_third(max)
+  local r = hs.geometry.rect(max.x, max.y, max.w, max.h)
+  r.x = math.floor(max.x + max.w / 3 * 2)
+  r.w = math.floor(max.w / 3)
+  return r
+end
+
+-- 2/3 of the left side
+function lib.window:first_two_thirds(max)
+  local r = hs.geometry.rect(max.x, max.y, max.w, max.h)
+  r.w = math.floor(max.w / 3 * 2)
+  return r
+end
+
+-- 2/3 of the right side
+function lib.window:last_two_thirds(max)
+  local r = hs.geometry.rect(max.x, max.y, max.w, max.h)
+  r.x = math.floor(max.x + max.w / 3)
+  r.w = math.floor(max.w / 3 * 2)
+  return r
+end
+
+function lib.window:copyRect(f, new)
+  f.x = new.x
+  f.y = new.y
+  f.w = new.w
+  f.h = new.h
+end
+
+-- Right half, 1/3, 2/3
+hs.hotkey.bind({ 'alt', 'ctrl' }, 'Right', function()
+  lib.window:resizeWindow(function(f, max)
+    if hs.geometry.equals(f, lib.window:last_half(max)) then -- 1/2 -> 1/3
+      -- debug
+      lib.window:copyRect(f, lib.window:last_third(max))
+    elseif hs.geometry.equals(f, lib.window:last_third(max)) then -- 1/3 -> 2/3
+      lib.window:copyRect(f, lib.window:last_two_thirds(max))
+    else                                                          -- 2/3 or any -> 1/2
+      lib.window:copyRect(f, lib.window:last_half(max))
+    end
+  end)
+end)
+
+-- Left half, 1/3, 2/3
+hs.hotkey.bind({ 'alt', 'ctrl' }, 'Left', function()
+  lib.window:resizeWindow(function(f, max)
+    if hs.geometry.equals(f, lib.window:first_half(max)) then      -- 1/2 -> 1/3
+      lib.window:copyRect(f, lib.window:first_third(max))
+    elseif hs.geometry.equals(f, lib.window:first_third(max)) then -- 1/3 -> 2/3
+      lib.window:copyRect(f, lib.window:first_two_thirds(max))
+    else                                                           -- 2/3 or any -> 1/2
+      lib.window:copyRect(f, lib.window:first_half(max))
+    end
+  end)
+end)
+
+-- Maximize
+hs.hotkey.bind({ 'alt', 'ctrl' }, 'Up', function()
+  lib.window:resizeWindow(function(f, max)
+    lib.window:copyRect(f, max)
+  end)
+end)
+
+-- move the window to the next screen
+hs.hotkey.bind({ 'alt', 'ctrl' }, 'n', function()
+  local win = hs.window.focusedWindow()
+  win:moveToScreen(win:screen():next())
+end)
