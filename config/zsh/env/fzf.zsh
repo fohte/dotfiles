18f8a19f438236039b06224fe39b8e2f61719189
Hayato Kawai <fohte.hk@gmail.com>
2024-06-23 14:20:13 +0900

    zsh: show preview files/dirs on the fzf window

--- config/zsh/env/fzf.zsh
+++ config/zsh/env/fzf.zsh
@@ -1,7 +1,7 @@
 export FZF_DEFAULT_COMMAND='rg --files --hidden --no-messages'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 has fd && export FZF_ALT_C_COMMAND='fd . -t d'
-export FZF_DEFAULT_OPTS='--cycle --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all'
+export FZF_DEFAULT_OPTS='--cycle --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all --preview "fzf-preview {}"'
 
 if [ -n "$TMUX" ]; then
   export FZF_TMUX=1
