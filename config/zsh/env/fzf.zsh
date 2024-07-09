4144fce85406465f10a3dda41bb82eabe8a2f1cd
Hayato Kawai <fohte.hk@gmail.com>
2024-07-09 11:54:12 +0900

    fzf: show full-command with preview

--- config/zsh/env/fzf.zsh
+++ config/zsh/env/fzf.zsh
@@ -1,7 +1,10 @@
 export FZF_DEFAULT_COMMAND='rg --files --hidden --no-messages'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 has fd && export FZF_ALT_C_COMMAND='fd . -t d'
-export FZF_DEFAULT_OPTS='--cycle --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all --preview "fzf-preview {}"'
+export FZF_DEFAULT_OPTS='--cycle --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all'
+export FZF_CTRL_T_OPTS='--preview "fzf-preview file {}"'
+export FZF_ALT_C_OPTS='--preview "fzf-preview dir {}"'
+export FZF_CTRL_R_OPTS='--preview "fzf-preview command {}" --preview-window=wrap'
 
 if [ -n "$TMUX" ]; then
   export FZF_TMUX=1
