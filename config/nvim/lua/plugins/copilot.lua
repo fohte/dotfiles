633867f71c34b2cff230c6a85defde17f2f5ac71
Hayato Kawai <fohte.hk@gmail.com>
2024-08-26 14:02:35 +0900

    nvim: add CopilotChat.nvim

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,12 +1,26 @@
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  config = function()
-    require('copilot').setup({
-      filetypes = {
-        ['*'] = true,
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup({
+        filetypes = {
+          ['*'] = true,
+        },
+      })
+    end,
+  },
+  {
+    {
+      'CopilotC-Nvim/CopilotChat.nvim',
+      branch = 'canary',
+      dependencies = {
+        { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+        { 'nvim-lua/plenary.nvim' },  -- for curl, log wrapper
       },
-    })
-  end,
+      build = 'make tiktoken',        -- Only on MacOS or Linux
+      opts = {},
+    },
+  },
 }
