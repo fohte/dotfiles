677fd382202b4935a82f02214456fba750518f30
Hayato Kawai <fohte.hk@gmail.com>
2024-10-01 14:24:06 +0900

    nvim: add keymaps for CopilotChat.nvim

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -17,10 +17,36 @@ return {
       branch = 'canary',
       dependencies = {
         { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-        { 'nvim-lua/plenary.nvim' },  -- for curl, log wrapper
+        { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+        { 'nvim-telescope/telescope.nvim' }, -- integrate with telescope
       },
-      build = 'make tiktoken',        -- Only on MacOS or Linux
-      opts = {},
+      build = 'make tiktoken', -- Only on MacOS or Linux
+      config = function()
+        require('CopilotChat').setup({
+          window = {
+            layout = 'float',
+            relative = 'editor',
+          },
+        })
+      end,
+      keys = {
+        {
+          '<Leader>ec',
+          function()
+            require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions())
+          end,
+        },
+        {
+          '<Leader>ca',
+          function()
+            local input = vim.fn.input('Copilot Chat: ')
+            if input ~= '' then
+              require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+            end
+          end,
+          mode = { 'n', 'v' },
+        },
+      },
     },
   },
 }
