baeb07e267eda9091fd712ca97f0bd5e20edc4da
Hayato Kawai <fohte.hk@gmail.com>
2024-11-04 23:10:20 +0900

    scripts: add is_wsl

--- scripts/utils.bash
+++ scripts/utils.bash
@@ -1,7 +1,29 @@
+#!/usr/bin/env bash
+
+pc_env=''
+
+case "$OSTYPE" in
+  darwin*)
+    pc_env='macos'
+    ;;
+  linux*)
+    if grep -q microsoft /proc/version; then
+      pc_env='wsl'
+    else
+      pc_env='linux'
+    fi
+    ;;
+  *) pc_env='other' ;;
+esac
+
+is_wsl() {
+  [[ $pc_env == wsl ]]
+}
+
 is_macos() {
-  [[ $OSTYPE == darwin* ]]
+  [[ $pc_env == macos ]]
 }
 
 is_linux() {
-  [[ $OSTYPE == linux* ]]
+  [[ $pc_env == linux ]]
 }
