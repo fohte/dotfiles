c93263b052671896b8ff462cf6366c50525c3621
Hayato Kawai <fohte.hk@gmail.com>
2024-12-22 23:41:35 +0900

    nvim/octo: disable default mappings
    avoid the <C-r> mapping

--- config/nvim/lua/plugins/octo.lua
+++ config/nvim/lua/plugins/octo.lua
@@ -1,7 +1,15 @@
 return {
   'pwntester/octo.nvim',
   config = function()
-    require('octo').setup()
+    require('octo').setup({
+      mappings_disable_default = true,
+      mappings = {
+        issue = {
+          open_in_browser = { lhs = '<C-b>', desc = 'open issue in browser' },
+          add_comment = { lhs = '<localleader>ca', desc = 'add comment' },
+        },
+      },
+    })
 
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'octo',
