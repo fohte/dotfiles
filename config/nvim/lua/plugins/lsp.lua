b543a75bc74e17c1e0cd76677fd74ad020d41211
Hayato Kawai <fohte.hk@gmail.com>
2024-12-31 18:15:36 +0900

    nvim: add lsp_lines.nvim

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -227,4 +227,15 @@ return {
     version = 'v1.x.x', -- version is optional, but recommended
     dependencies = { 'neovim/nvim-lspconfig' },
   },
+  {
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+    config = function()
+      require('lsp_lines').setup()
+
+      -- Disable virtual_text since it's redundant due to lsp_lines.
+      vim.diagnostic.config({
+        virtual_text = false,
+      })
+    end,
+  },
 }
