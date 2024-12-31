a013e04c1946beca7512aba6f0fcd7735d940436
Hayato Kawai <fohte.hk@gmail.com>
2024-12-31 17:15:00 +0900

    nvim: remove nvim-tresitter-context
    ウィンドウの高さがとられるだけであまり活用できていない

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,10 +7,6 @@ return {
       -- automatically close end statements (e.g. end in ruby/lua, fi in bash)
       'RRethy/nvim-treesitter-endwise',
 
-      -- keep showing the current context on the top of the window
-      -- e.g. if you're inside an if statement, it will show "if ~~" on the top
-      'nvim-treesitter/nvim-treesitter-context',
-
       -- automatically set the commentstring based on the current context
       -- this plugin is used to determine the comment character in comment.nvim
       'JoosepAlviste/nvim-ts-context-commentstring',
