ca32e4e258ae63e02f122ed5a70199ee65f87b19
Hayato Kawai <fohte.hk@gmail.com>
2025-04-29 02:32:12 +0900

    hammerspoon: remove yabai bindings
    stop using yabai

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,52 +1,63 @@
-local lib = require('lib')
-local yabai = require('rc/yabai')
-
-local function defineOpenAppHotkey(mods, key, func)
-  hs.hotkey.bind(mods, key, function()
-    yabai.with_temp_config({
-      focus_follows_mouse = 'off',
-      mouse_follows_focus = 'on',
-    }, func)
-  end)
-end
-
-defineOpenAppHotkey({ 'alt' }, '1', function()
-  local window_id = yabai.find_next_window('WezTerm').id
-  if window_id == nil then
-    hs.application.launchOrFocus('/Applications/WezTerm.app')
-  else
-    yabai.run('window --focus ' .. window_id)
-  end
+------------------------------
+--- Applications
+------------------------------
+hs.hotkey.bind({ 'alt' }, '1', function()
+  hs.application.launchOrFocus('/Applications/WezTerm.app')
 end)
 
-defineOpenAppHotkey({ 'alt' }, '2', function()
+hs.hotkey.bind({ 'alt' }, '2', function()
   hs.application.launchOrFocus('/Applications/Arc.app')
 end)
 
-defineOpenAppHotkey({ 'alt' }, '5', function()
+hs.hotkey.bind({ 'alt' }, '5', function()
   hs.application.launchOrFocus('/Applications/ChatGPT.app')
 end)
 
-defineOpenAppHotkey({ 'alt' }, 'o', function()
+hs.hotkey.bind({ 'alt' }, 'o', function()
   hs.application.launchOrFocus('/Applications/Obsidian.app')
 end)
 
-defineOpenAppHotkey({ 'alt' }, '8', function()
+hs.hotkey.bind({ 'alt' }, '8', function()
   hs.application.launchOrFocus('/Applications/Todoist.app')
 end)
 
-defineOpenAppHotkey({ 'alt' }, '9', function()
+hs.hotkey.bind({ 'alt' }, '9', function()
   hs.application.launchOrFocus('/Applications/Fantastical.app')
 end)
 
--- WezTerm の GhostText window を開く
-defineOpenAppHotkey({ 'alt' }, '4', function()
-  local window_id = find_window_id('wezterm-gui')
-  print('Alt-4: ' .. hs.inspect(window_id))
+------------------------------
+--- Window Management
+------------------------------
+-- disable animation
+hs.window.animationDuration = 0
 
-  if window_id == nil then
-    hs.execute([[ nohup wezterm start -- zsh -c 'nvim -c GhostTextStart' > /dev/null 2>&1 & ]], true)
-  else
-    yabai.run('window --focus ' .. window_id)
+-- maximize/restore window size
+local windowSizes = {}
+hs.hotkey.bind({ 'ctrl', 'shift' }, 't', function()
+  local window = hs.window.focusedWindow()
+  local id = window:id()
+
+  if windowSizes[id] then -- restore window size
+    window:setFrame(windowSizes[id])
+    windowSizes[id] = nil
+  else -- maximize window size
+    windowSizes[id] = window:frame()
+    window:maximize()
   end
+end)
+
+-- left half
+hs.hotkey.bind({ 'ctrl', 'shift' }, 'Left', function()
+  local window = hs.window.focusedWindow()
+  local screen = window:screen()
+  local frame = screen:frame()
+  window:setFrame(hs.geometry.rect(frame.x, frame.y, frame.w / 2, frame.h))
+end)
+
+-- right half
+hs.hotkey.bind({ 'ctrl', 'shift' }, 'Right', function()
+  local window = hs.window.focusedWindow()
+  local screen = window:screen()
+  local frame = screen:frame()
+  window:setFrame(hs.geometry.rect(frame.x + frame.w / 2, frame.y, frame.w / 2, frame.h))
 end)
