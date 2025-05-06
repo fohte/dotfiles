fc3aefef6816e2ae2f2b51c10d364bf0e2359715
Hayato Kawai <fohte.hk@gmail.com>
2025-05-06 19:02:51 +0900

    nvim: enable copilot on text files

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -7,10 +7,6 @@ return {
       require('copilot').setup({
         filetypes = {
           ['*'] = true,
-
-          -- disable copilot on text files
-          ['markdown'] = false,
-          -- ['review'] = false,
         },
       })
     end,
