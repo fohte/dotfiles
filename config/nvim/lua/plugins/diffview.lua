24234bf9032b859316258dd825294ffdd506b272
Hayato Kawai <fohte.hk@gmail.com>
2025-06-14 23:48:16 +0900

    nvim/diffview: map q to close the diffview window

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -1,9 +1,11 @@
 return {
   'sindrets/diffview.nvim',
   config = function()
-    require('diffview').setup({
-      -- base: https://github.com/sindrets/diffview.nvim/pull/258#issuecomment-1408689220
+    local diffview = require('diffview')
+
+    diffview.setup({
       hooks = {
+        -- base: https://github.com/sindrets/diffview.nvim/pull/258#issuecomment-1408689220
         diff_buf_win_enter = function(bufnr, winid, ctx)
           if ctx.layout_name:match('^diff2') then
             if ctx.symbol == 'a' then
@@ -22,6 +24,29 @@ return {
             end
           end
         end,
+      },
+      keymaps = {
+        view = {
+          { 'n', 'q', diffview.close, { desc = 'Close the diffview' } },
+        },
+        diff1 = {
+          { 'n', 'q', diffview.close, { desc = 'Close the diffview' } },
+        },
+        diff2 = {
+          { 'n', 'q', diffview.close, { desc = 'Close the diffview' } },
+        },
+        diff3 = {
+          { 'n', 'q', diffview.close, { desc = 'Close the diffview' } },
+        },
+        diff4 = {
+          { 'n', 'q', diffview.close, { desc = 'Close the diffview' } },
+        },
+        file_panel = {
+          { 'n', 'q', diffview.close, { desc = 'Close the diffview' } },
+        },
+        file_history_panel = {
+          { 'n', 'q', diffview.close, { desc = 'Close the diffview' } },
+        },
       },
     })
   end,
